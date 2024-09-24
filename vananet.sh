f2d92d5406b94d8c18d90a69f984f506312a7d5d
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-09-24 16:23:24 +0300

    update 1.0

--- vananet.sh
+++ vananet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="1.1.1"
+VERSION="1.0"
 # Виведення версії при запуску
 echo -e "\e[1;32mScript version: $VERSION\e[0m"  # Зелений текст
 # Функція для перевірки на порожні значення
@@ -41,246 +41,83 @@ while true; do
       "Pre Install")
                 echo -e "\e[1;34mStarting Pre Install...\e[0m"  # Синій текст
 
-        # Оновлення пакетів
-        sudo apt update && sudo apt upgrade -y
-
-        # Встановлення git
-        sudo apt-get install git -y
-        git --version || { echo -e "\e[1;31mGit installation failed\e[0m"; exit 1; }
-
-        # Встановлення Python
-        sudo apt install software-properties-common -y
-        sudo add-apt-repository ppa:deadsnakes/ppa -y
-        sudo apt update
-        sudo apt install python3.11 python3-pip python3-venv curl -y
-        python3.11 --version || { echo -e "\e[1;31mPython installation failed\e[0m"; exit 1; }
-
-        # Встановлення Poetry
-        if ! command -v poetry &> /dev/null; then
-            echo -e "\e[1;32mInstalling Poetry...\e[0m"  # Зелений текст
-            sudo apt install python3-poetry -y || {
-                echo -e "\e[1;31mPoetry installation via apt failed. Trying curl...\e[0m"
-                curl -sSL https://install.python-poetry.org | python3 - || {
-                    echo -e "\e[1;31mPoetry installation via curl failed.\e[0m"
-                    exit 1
-                }
-            }
-        else
-            echo -e "\e[1;32mPoetry is already installed.\e[0m"
-        fi
-
-        # Додати Poetry до PATH, якщо ще не додано
-        if ! grep -q "$HOME/.local/bin" ~/.bashrc; then
-            echo "export PATH=\"$HOME/.local/bin:\$PATH\"" >> ~/.bashrc
-            source ~/.bashrc
-            hash -r
-        fi
-
-        # Перевірка версії Poetry
-        poetry --version || { echo -e "\e[1;31mPoetry installation failed\e[0m"; exit 1; }
-
-        # Встановлення Node.js за допомогою nvm
-        if ! command -v nvm &> /dev/null; then
-            echo -e "\e[1;32mInstalling nvm...\e[0m"
-            curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-            source ~/.bashrc
-            source "$HOME/.nvm/nvm.sh"  # Завантажити nvm
-        fi
-
-        # Встановити останню версію Node.js
-        nvm install 18
-        nvm use 18
-
-        # Перевірка встановлення Node.js
-        node -v || { echo -e "\e[1;31mNode.js installation failed\e[0m"; exit 1; }
-        npm -v || { echo -e "\e[1;31mnpm installation failed\e[0m"; exit 1; }
-
-        # Встановлення Yarn глобально
-        npm install -g yarn
-        yarn --version || { echo -e "\e[1;31mYarn installation failed\e[0m"; exit 1; }
-
-        echo -e "\e[1;32mPre Install completed.\e[0m"  # Зелений текст
+        #docker + compose
+          touch $HOME/.bash_profile
+            cd $HOME
+            if ! docker --version; then
+              sudo apt update
+              sudo apt upgrade -y
+              sudo apt install curl jq apt-transport-https ca-certificates gnupg lsb-release -y
+              . /etc/*-release
+              wget -qO- "https://download.docker.com/linux/${DISTRIB_ID,,}/gpg" | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+              echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+              sudo apt update
+              sudo apt install docker-ce docker-ce-cli containerd.io -y
+              docker_version=`apt-cache madison docker-ce | grep -oPm1 "(?<=docker-ce \| )([^_]+)(?= \| https)"`
+              sudo apt install docker-ce="$docker_version" docker-ce-cli="$docker_version" containerd.io -y
+            fi
+            if ! docker compose version; then
+              sudo apt update
+              sudo apt upgrade -y
+              sudo apt install wget jq -y
+              local docker_compose_version=`wget -qO- https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name"`
+              sudo wget -O /usr/bin/docker-compose "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-`uname -s`-`uname -m`"
+              sudo chmod +x /usr/bin/docker-compose
+              . $HOME/.bash_profile
+            fi
 
         break
         ;;
         
       "Install Node")
         echo -e "\e[1;34mStarting Node Installation...\e[0m"  # Синій текст
-        export PATH="$HOME/.local/bin:$PATH"
         cd $HOME
-        # Clone repository
-        git clone https://github.com/vana-com/vana-dlp-chatgpt.git
-        cd vana-dlp-chatgpt
+        git clone https://github.com/vana-com/vana.git
+        cd $HOME/vana
         cp .env.example .env
-
-        if ! command -v poetry &> /dev/null; then
-            echo -e "\e[1;31mPoetry is not installed. Installing now...\e[0m"
-            curl -sSL https://install.python-poetry.org | python3 -
-            export PATH="$HOME/.local/bin:$PATH"
-            source ~/.bashrc
-        fi
-
-        poetry install
-        pip install vana
-
-        # Генерація гаманця
-        vanacli wallet create --wallet.name default --wallet.hotkey default
-        sleep 3
-
-        # Експорт ключів
-        vanacli wallet export_private_key --wallet.name default
-        sleep 4
-        vanacli wallet export_private_key --wallet.name default --wallet.hotkey default
-
-        # Генерація ключів
-        chmod +x keygen.sh
-        ./keygen.sh
+        while true; do
+          WA="" PK=""
+          check_empty WA "WITHDRAWAL_ADDRESS: "
+          check_empty PK "DEPOSIT_PRIVATE_KEY: "
+          confirm_input WA PK
+          if [ $? -eq 0 ]; then break; fi
+        done
+        sed -i "s/0x0000000000000000000000000000000000000000/$WA/" .env
+        sed -i "s/0000000000000000000000000000000000000000000000000000000000000000/$PK/" .env
+        sed -i "s/USE_VALIDATOR=false/USE_VALIDATOR=true/" .env
         cd $HOME
-
         echo -e "\e[1;32mNode Installation completed.\e[0m"
         break
         ;;
-
-      "Deploy")
-        echo -e "\e[1;34mStarting Deployment...\e[0m"  # Синій текст
-        cd $HOME
-        export PATH="$HOME/.local/bin:$PATH"
-        cd $HOME
-        git clone https://github.com/vana-com/vana-dlp-smart-contracts.git
-        cd vana-dlp-smart-contracts
-
-        if ! command -v yarn &> /dev/null; then
-            echo -e "\e[1;31mYarn is not installed. Installing now...\e[0m"
-            npm install -g yarn
-        fi
-
-        yarn install
-        cp .env.example .env
-
-        while true; do
-          DPK="" OA="" DN="" DTN="" DTS=""
-          check_empty DPK "Coldkey_PRIVATE_KEY: "
-          check_empty OA "Coldkey_ADDRESS: "
-          check_empty DN "Data Liquidity Pool NAME: "
-          check_empty DTN "Data Liquidity Pool TOKEN NAME: "
-          check_empty DTS "Data Liquidity Pool TOKEN SYMBOL: "
-          confirm_input DPK OA DN DTN DTS
-          if [ $? -eq 0 ]; then break; fi
-        done
-
-        sed -i "s/0x71000000000000000000000etc/$DPK/" .env
-        sed -i "s/0x00etc/$OA/" .env
-        sed -i "s/Custom Data Liquidity Pool/$DN/" .env
-        sed -i "s/Custom Data Autonomy Token/$DTN/" .env
-        sed -i "s/CUSTOMDAT/$DTS/" .env
-
-        npx hardhat deploy --network satori --tags DLPDeploy
-        cd $HOME
-
-        echo -e "\e[1;32mDeployment completed.\e[0m"
-        break
-        ;;
-
-      "Pre Validator")
-        echo -e "\e[1;34mStarting Pre Validator Configuration...\e[0m"  # Синій текст
-        cd $HOME/vana-dlp-chatgpt
-        while true; do
-          OAK="" DLP="" DLPT=""
-          check_empty OAK "OPENAI_API_KEY: "
-          check_empty DLP "Data Liquidity Pool CONTRACT: "
-          check_empty DLPT "Data Liquidity Pool TOKEN CONTRACT: "
-          confirm_input OAK DLP DLPT
-          if [ $? -eq 0 ]; then break; fi
-        done
-
-        KEY=$(cat $HOME/vana-dlp-chatgpt/public_key_base64.asc)
-
-        cat << EOF > $HOME/vana-dlp-chatgpt/.env
-# The network to use, currently Vana Satori testnet
-OD_CHAIN_NETWORK=satori
-OD_CHAIN_NETWORK_ENDPOINT=https://rpc.satori.vana.org
-
-# Optional: OpenAI API key for additional data quality check
-OPENAI_API_KEY="$OAK"
-
-# Optional: Your own DLP smart contract address once deployed to the network, useful for local testing
-DLP_SATORI_CONTRACT="$DLP"
-
-# Optional: Your own DLP token contract address once deployed to the network, useful for local testing
-DLP_TOKEN_SATORI_CONTRACT="$DLPT"
-
-# The private key for the DLP, follow "Generate validator encryption keys" section in the README
-PRIVATE_FILE_ENCRYPTION_PUBLIC_KEY_BASE64="$KEY"
-EOF
-
-        echo -e "\e[1;32mPre Validator Configuration completed.\e[0m"
-        cd $HOME
-        break
-        ;;
-
-      "Stake Validator")
-        echo -e "\e[1;34mStarting Validator Staking...\e[0m"  # Синій текст
-        cd $HOME/vana-dlp-chatgpt
-        ./vanacli dlp register_validator --stake_amount 10
-        sleep 5
-
-        read -p "Hotkey ADDRESS : " HOT
-        ./vanacli dlp approve_validator --validator_address=$HOT
-        sleep 5
-
-        echo -e "\e[1;32mValidator Staking completed.\e[0m"
-        echo $(which poetry)
-
-        sudo tee /etc/systemd/system/vana.service << EOF
-[Unit]
-Description=Vana Validator Service
-After=network.target
-
-[Service]
-Type=simple
-User=root
-WorkingDirectory=/root/vana-dlp-chatgpt
-ExecStart=/root/.local/bin/poetry run python -m chatgpt.nodes.validator
-Restart=on-failure
-RestartSec=10
-Environment=PATH=/root/.local/bin:/usr/local/bin:/usr/bin:/bin:/root/vana-dlp-chatgpt/myenv/bin
-Environment=PYTHONPATH=/root/vana-dlp-chatgpt
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-        sudo systemctl daemon-reload
-        sudo systemctl enable vana.service
-        sudo systemctl start vana.service
-        sudo systemctl status vana.service
+      
+      "RUN Validator")
+        echo -e "\e[1;34mStarting Validator...\e[0m"  # Синій текст
+        docker compose -f $HOME/vana/docker-compose.yml --profile manual run --rm validator-keygen
+        echo -e "\e[1;34mGenerate validator keys DONE!\e[0m"  # Синій текст
+        docker compose -f $HOME/vana/docker-compose.yml --profile init --profile manual run --rm submit-deposits
+        echo -e "\e[1;34mSSubmit deposits for your validator.\e[0m"  # Синій текст
+        docker compose -f $HOME/vana/docker-compose.yml --profile init --profile validator up -d
         break
         ;;
 
       "Logs")
         echo -e "\e[1;34mDisplaying Logs...\e[0m"  # Синій текст
-        sudo journalctl -u vana.service -f
+        docker compose -f $HOME/vana/docker-compose.yml logs -f
         break
         ;;
 
       "Uninstall")
         echo -e "\e[1;34mUninstallation process started...\e[0m"  # Синій текст
-        if [ ! -d "$HOME/vana-dlp-chatgpt" ]; then
-          echo -e "\e[1;31mDirectory $HOME/vana-dlp-chatgpt does not exist.\e[0m"
+        if [ ! -d "$HOME/vana" ]; then
+          echo -e "\e[1;31mDirectory $HOME/vana does not exist.\e[0m"
           continue
         fi
 
         read -r -p "Uninstall ? [y/N] " response
         case "$response" in
           [yY][eE][sS]|[yY]) 
-            rm -rf $HOME/vana-dlp-chatgpt
-            rm -rf $HOME/vana-dlp-smart-contracts
-
-            sudo systemctl stop vana.service
-            sudo systemctl disable vana.service
-            sudo systemctl daemon-reload
-            sudo rm /etc/systemd/system/vana.service
-
+            docker compose -f $HOME/vana/docker-compose.yml --profile init --profile validator down -v
+            rm -rf $HOME/vana
             echo -e "\e[1;32mUninstallation completed.\e[0m"
             ;;
           *)
